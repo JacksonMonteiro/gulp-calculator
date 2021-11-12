c586736badc09b3b20b51b46f1dadff73535d5c0
jacksonmonteiro <infor.jackson324@gmail.com>
2021-11-12 18:13:04 -0300

    gulpfiles tasks and config

--- gulpfiles.js
+++ gulpfiles.js
@@ -1,1 +1,61 @@
-  
+const {
+    src,
+    dest,
+    parallel
+} = require('gulp');
+
+const rename = require('gulp-rename');
+const minifyJS = require('gulp-uglify');
+const minifyCSS = require('gulp-uglifycss]');
+
+const sass = require('gulp-sass')(require('sass'));
+sass.compiler = require('node-sass');
+
+const babel = require('gulp-babel');
+const cssImporter = require('gulp-cssimport');
+
+function base() {
+    return src('src/templates/*.html')
+    .pipe(
+        dest('public/')
+    );
+}
+
+function js() {
+    return src('src/js/*.js')
+    .pipe(
+        babel({
+            presets: [ '@babel/env' ]
+        })
+    )
+    .pipe(
+        minifyJS()
+    )
+    .pipe(
+        rename({extname: '.min.js'})
+    )
+    .pipe(
+        dest('public/assets/js')
+    );
+}
+
+function css() {
+    return src('src/css/*.css')
+    .pipe(
+        cssImporter()
+    )
+    .pipe(
+        sass()
+    )
+    .pipe(
+        minifyCSS()
+    )
+    .pipe(
+        rename({extname: '.min.css'})
+    )
+    .pipe(
+        dest('public/assets/css')
+    )
+}
+
+exports.default = parallel(base, css, js);
