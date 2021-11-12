26b45cb0ce358e775883a5d2ab20e0b5f81c082a
jacksonmonteiro <infor.jackson324@gmail.com>
2021-11-12 19:37:22 -0300

    sum function

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+const calcScreen = document.querySelector('.calculator__screen');
+const numbers = document.querySelectorAll('.calculator__number');
+
+const plus = document.querySelector('#plusButton');
+const equal  = document.querySelector('#equalButton');
+
+let firstNumber = 0;
+let secondNumber = 0;
+
+alert('Funcionando');
+
+// Got the number when clicked
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', () => {
+        if (calcScreen.innerHTML == 0) {
+            calcScreen.innerHTML = numbers[i].innerHTML;
+        } else {
+            calcScreen.innerHTML += numbers[i].innerHTML;
+        }
+    });
+}
+
+// Got the first number typed and wait the second for make the calc
+plus.addEventListener('click', ()=> {
+    if (calcScreen.innerHTML != '' && calcScreen.innerHTML != 0) {
+        firstNumber = Number.parseInt(calcScreen.innerHTML);
+        calcScreen.innerHTML = 0;
+    }
+})
+
+
+// Make the operation
+equal.addEventListener('click', () => {
+    if (firstNumber != '' && firstNumber != 0 && calcScreen != '' && calcScreen != 0) {
+        secondNumber = Number.parseInt(calcScreen.innerHTML);
+        calcScreen.innerHTML = firstNumber + secondNumber;
+    }
+})
